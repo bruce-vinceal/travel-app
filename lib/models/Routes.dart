dded6dfd5f686963ef2c716d4c8a350f6af9f10f
bruce-vinceal <brucevinceal@gmail.com>
2024-02-28 23:34:24 +0800

    Updated routes

--- lib/models/Routes.dart
+++ lib/models/Routes.dart
@@ -8,13 +8,11 @@ import 'package:travel_app/screens/Home.dart';
 import 'package:travel_app/screens/Profile.dart';
 import 'package:travel_app/screens/Registration.dart';
 import 'package:travel_app/screens/Search.dart';
-import 'package:travel_app/screens/Settings.dart';
 
 final Map<String, WidgetBuilder> routes = {
   LoginScreen.routeName: (BuildContext context) => const LoginScreen(),
   Dashboard.routeName: (BuildContext context) => const Dashboard(),
   Details.routeName: (BuildContext context) => const Details(),
-  Settings.routeName: (BuildContext context) => const Settings(),
   Search.routeName: (BuildContext context) => const Search(),
   Profile.routeName: (BuildContext context) => const Profile(),
   Home.routeName: (BuildContext context) => const Home(),
